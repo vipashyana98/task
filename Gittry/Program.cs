8a0d9d6f613f047d13ae8835a7c74ee17de453fc
vipashyana <Vipashyana.Sharma@lntinfotech.com>
2020-09-09 17:48:19 +0530

    committed

---
+++ Gittry/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Gittry
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            
+            
+                int num1, num2;
+            
+            
+                Console.WriteLine("please enter the Number1");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter the Number2");
+                num2 = Convert.ToInt32(Console.ReadLine());
+            
+            
+                int result = num1 + num2;
+            
+                Console.WriteLine("Sum of two Numbers"+ result.ToString());
+                Console.ReadLine();
+            
+            
+
+        }
+    }
+}
